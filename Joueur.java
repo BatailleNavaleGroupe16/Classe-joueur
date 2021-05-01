2f5b55a052fb51b969cbf4f661ec93b1bd11eceb
antdulac <83459720+antdulac@users.noreply.github.com>
2021-05-01 18:06:33 +0200

    Update Joueur.java

--- Joueur.java
+++ Joueur.java
@@ -4,7 +4,7 @@ public class Joueur{
     
     public joueur() {
         this.grille = new int [10][10];//chaque case va prendre une valeur entre 0 et 5 pour indiquer s'il n'y a rien ou le numéro du bateau
-        this.coupsPrecedents = new char [10][10]; 
+        this.coupsPrecedents = new int [10][10]; 
     }
     
     public void PlacementBateaux(Bateau b1){
@@ -21,20 +21,27 @@ public class Joueur{
     public void initialiseCoupsPrecedents(){
         for(int i = 0 ; i<coupsPrecedents.length() ; i++){
             for(int j = 0 ; j<coupsPrecedents[i].length() ; j++){
-                coupsPrecedents[i][j] = ".";
+                coupsPrecedents[i][j] = 0;
             }
         }
     }
     
     public void enregistreCoup(int x , int y){
-        coupsPrecedents[x][y] = "X";
+        coupsPrecedents[x][y] = 1;
     }
     
-    public void afficheCoupsPrecedents(){
+    public void afficheGrille(){
+        System.out.println("Coups joués depuis le début de la partie par vous : ");
         for(int i = 0 ; i<coupsPrecedents.length() ; i++){
             for(int j = 0 ; j<coupsPrecedents[i].length() ; j++){
-                System.out.println(" "+coupsPrecedents[i][j]+" ");
+                if(coupsPrecedents[i][j] == 0){
+                    System.out.print(" . ");
+                }else{
+                    System.out.print(" X ")
+                }
+                
             }
+            System.out.println();
         }
     }
 }
